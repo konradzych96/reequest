040de7792cc44e938c6c073fc79f863c486a40ed
konradzych96 <konradzych96@gmail.com>
2022-07-25 20:33:43 +0200

    scroll event

--- src/client/common.js
+++ src/client/common.js
@@ -1,5 +1,17 @@
-import Parallax from "parallax-js";
-import "parallax-background-image";
+import * as $ from 'jquery'
 
-var scene = document.getElementById('p1');
-var parallaxInstance = new Parallax(scene);
+$(window).scroll(function(event) {
+
+    if($('#content-brick1').length){
+
+    if ($('#content-brick1').offset().top - $(window).outerHeight() < $(window).scrollTop()) {
+        $("#content-brick1").css("background-size", (100 + 100 * $(window).scrollTop() / 15000) + "%");
+    }
+    if ($('#content-brick2').offset().top - $(window).outerHeight() < $(window).scrollTop()) {
+        $("#content-brick2").css("background-size", (100 + 100 * $(window).scrollTop() / 15000) + "%");
+    }
+    if ($('#content-brick3').offset().top - $(window).outerHeight() < $(window).scrollTop()) {
+        $("#content-brick3").css("background-size", (100 + 100 * $(window).scrollTop() / 15000) + "%");
+    }
+}
+}); 
